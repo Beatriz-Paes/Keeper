d31cbbb64a6dbcd5abed7e5e15ca1cce53d35839
beah <beah.dev@gmail.com>
2020-09-01 05:39:34 -0400

    Estilizaçao da home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import keeper from '../../assets/images/home/keeper.svg';
-
+import keeperLogo from '../../assets/images/keeper_logo.svg';
+import '../style.css';
 
 
 function Home() {
@@ -8,16 +9,21 @@ function Home() {
     <div id="page-home">
         <div id="page-home-content" className="container">
           <div className="logo-container">
-            <h1>Bem vindo(a) ao Keeper</h1>
+            <img src={keeperLogo} alt="logo"/>
             <h2>Imagine uma nova história para sua vida e acredite nela!</h2>
+          </div>
 
             <img src={keeper} alt="Keeper" className="hero-image"/>
 
             <div className="buttons-container">
-              <a href="" className="Login">Login</a>
-
+              <a href="" className="login">Login</a>
+              <a href="" className="register">Registre-se</a>            
             </div>
-          </div>
+
+            <span className="total-connections">
+              Total de 200 pessoas já conectadas <img src="" alt=""/>
+            </span>
+
         </div>
     </div>
 
