b74ce6a5bf1a384753c30b2963e5176d451ce68f
ldantas-s <lucianostudant@gmail.com>
2020-10-27 00:00:18 -0300

    Atualizar o estilo da landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -8,62 +8,58 @@ import bemestar from '../../assets/images/home/bemestar.svg';
 import keeper from '../../assets/images/home/keeper.svg';
 import heart from '../../assets/images/home/heart.svg';
 import keeperLogo from '../../assets/images/keeper_logo.svg';
-import '../style.css';
+import '../../assets/style/page/landing.css';
 
 
 function Landing() {
   return (
     <div id="page-landing">
-        <div id="page-landing-content" className="container">
-          <div className="top-nav">
-            <Link to="/Home" className="start">Início</Link>
-            <Link to="" className="guardian">Seja um guardião</Link>
-            <Link to="" className="">Sobre</Link>
-          </div>
-          <div className="logo-container">
-            <img src={keeperLogo} alt="logo"/>
-            <h2>Imagine uma nova história para sua vida e acredite nela!</h2>
-          </div>
+      <header className="top-nav">
+        <Link to="/Home" className="start">Início</Link>
+        <Link to="" className="guardian">Seja um guardião</Link>
+        <Link to="" className="">Sobre</Link>
+      </header>
+      
+      <div className="logo-container">
+        <img src={keeperLogo} alt="logo"/>
+        <h2>Imagine uma nova história para sua vida e acredite nela!</h2>
+      </div>
 
-          <img src={keeper} alt="Keeper" className="hero-image"/>
+      <img src={keeper} alt="Keeper" className="hero-image"/>
 
-          <div className="activities-container">
-            <div className="image-container1">
-                <Link to="" className="saude">
-                <img src={saude} alt="Sua saúde" className="image-saude"/>
-                <h3>Sua saúde</h3>
-                </Link>
-
-                <Link to="" className="mente">
-                <img src={mente} alt="Sua saúde" className="image-mente"/>
-                <h3>Sua mente</h3>
-                </Link>
-          </div>
-
-
-            <div className="image-container2">
-              <Link to="" className="corpo">
-              <img src={corpo} alt="Seu corpo" className="image-corpo"/>
-              <h3>Seu corpo</h3>
-              </Link>
-
-              <Link to="" className="bemestar">
-              <img src={bemestar} alt="Seu bem-estar" className="image-bemestar"/>
-              <h3>Seu bem estar</h3>
-              </Link>
-            </div>
-          </div>
-
-            <div className="buttons-container">
-              <Link to="/Home" className="login">Login</Link>
-              <Link to="" className="register">Registre-se</Link>            
-            </div>
-
-            <span className="total-connections">
-              Total de 200 pessoas já conectadas <img src={heart} alt="coração azul"/>
-            </span>
-
+      <div className="activities">
+        <div className="activities-item">
+          <img src={saude} alt="Sua saúde" className="image-saude"/>
+          <Link to="">Sua saúde</Link>
         </div>
+
+        <div className="activities-item">
+          <img src={mente} alt="Sua saúde" className="image-mente"/>
+          <Link to="">Sua mente</Link>
+        </div>
+
+        <div className="activities-item">
+          <img src={corpo} alt="Seu corpo" className="image-corpo"/>
+          <Link to="">Seu corpo</Link>
+        </div>
+
+        <div className="activities-item">
+          <img src={bemestar} alt="Seu bem-estar" className="image-bemestar"/>
+          <Link to="">Seu bem estar</Link>
+        </div>
+      </div>
+
+      <div className="buttons-container">
+        <Link to="/Home" className="login">Login</Link>
+        <Link to="" className="register">Registre-se</Link>            
+      </div>
+
+      <div className="total-connections">
+        <div className="connections-content">
+          <p>Total de 200 pessoas já conectadas</p> <img src={heart} alt="coração azul"/>
+        </div>
+      </div>
+
     </div>
 
   );
